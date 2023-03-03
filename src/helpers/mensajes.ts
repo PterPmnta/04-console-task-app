b9b305e52c7b86fa2a9dcfd2bca7b32d9ea9611e
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2023-03-02 21:26:02 -0500

    add promise returned and other changes

--- src/helpers/mensajes.ts
+++ src/helpers/mensajes.ts
@@ -2,36 +2,45 @@ import 'colorts/lib/string';
 import * as readline from 'node:readline';
 
 export const showMenu = () => {
-    console.clear();
-    console.log('================================'.green);
-    console.log('      Seleccione una opcion     '.green);
-    console.log('================================\n'.green);
 
-    console.log(`${'1.'.green} Crear tarea`);
-    console.log('2. Listar tarea');
-    console.log('3. Listar tarea(s) completada(s)');
-    console.log('4. Listar tarea(s) pendiente(s)');
-    console.log('5. Completar tarea(s)');
-    console.log('6. Borrar tarea');
-    console.log('0. Salir \n');
+    return new Promise((resolve) => {
 
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    })
+        console.clear();
+        console.log('================================'.green);
+        console.log('      Seleccione una opcion     '.green);
+        console.log('================================\n'.green);
 
-    rl.question('Seleccione una opcion: ', (opt:any) => {
-        rl.close();
-    });
+        console.log(`${'1.'.green} Crear tarea`);
+        console.log(`${'2.'.green} Listar tarea`);
+        console.log(`${'3.'.green} Listar tarea(s) completada(s)`);
+        console.log(`${'4.'.green} Listar tarea(s) pendiente(s)`);
+        console.log(`${'5.'.green} Completar tarea(s)`);
+        console.log(`${'6.'.green} Borrar tarea`);
+        console.log(`${'0.'.green} Salir \n`);
+
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        })
+
+        rl.question('Seleccione una opcion: ', (opt:any) => {
+            rl.close();
+            resolve(opt);
+        });
+
+    })    
 }
 
 export const pauseApp = () => {
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    })
-
-    rl.question(`\n Presione ${'Enter'.green} para pausar la app \n`, (opt:any) => {
-        rl.close();
-    });
+    return new Promise((resolve) => {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        })
+    
+        rl.question(`\n Presione ${'Enter'.green} para pausar la app \n`, (opt:any) => {
+            rl.close();
+            resolve(opt);
+        });
+    })    
 }
