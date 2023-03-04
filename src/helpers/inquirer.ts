62b49481237adc58490fdc27e48a20d8e8f1d409
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2023-03-04 15:12:50 -0500

    new changes and new function

--- src/helpers/inquirer.ts
+++ src/helpers/inquirer.ts
@@ -6,7 +6,36 @@ const preguntas = [
         type: 'list',
         name: 'opcion',
         message: '¿Que desea hacer?',
-        choices: ['opt1', 'opt2', 'opt3']
+        choices: [
+            {
+                value: '1',
+                name: '1. Crear tarea'
+            },
+            {
+                value: '2',
+                name: '2. Listar tarea(s)'
+            },
+            {
+                value: '3',
+                name: '3. Listar tarea(s) completada(s)'
+            },
+            {
+                value: '4',
+                name: '4. Listar tarea(s) pendiente(s)'
+            },
+            {
+                value: '5',
+                name: '5. Completar tarea(s)'
+            },
+            {
+                value: '6',
+                name: '6. Borrar tarea'
+            },
+            {
+                value: '0',
+                name: '0. Salir'
+            }
+        ]
     }
 ]
 
@@ -17,7 +46,20 @@ export const inquirerMenu = async() => {
     console.log('      Seleccione una opcion     '.green);
     console.log('================================\n'.green);
 
-    const opt = await inquirer.prompt(preguntas);
+    const {opcion} = await inquirer.prompt(preguntas);
 
-    return opt;
+    return opcion;
 }
+
+export const pausarApp = async () => {
+
+    const preguntas = [
+        {
+            type: 'input',
+            name: 'enter',
+            message: `Presione ${'enter'.green} para continuar`
+        }
+    ]
+
+    await inquirer.prompt(preguntas);
+} 
