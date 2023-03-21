77f9cfcb2a674976bd2518adadfaa34c4d1bc7fb
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2023-03-20 21:35:58 -0500

    validate id and input to task to delete

--- src/app.ts
+++ src/app.ts
@@ -1,50 +1,65 @@
 import 'colorts/lib/string';
-import { inquirerMenu, leerInput, listTaskToDelete, pausarApp } from './helpers/inquirer';
+import {
+    confirmTaskToDelete,
+    inquirerMenu,
+    leerInput,
+    listTaskToDelete,
+    pausarApp
+} from './helpers/inquirer';
 import { Tarea } from './models/tarea';
 import { Tareas } from './models/tareas';
 import { loadDB, saveDB } from './helpers/saveFile';
 
 console.clear();
 const main = async () => {
-    
-    let opt: any = '' 
+    let opt: any = '';
     const tareas = new Tareas();
-    const readTasks = loadDB()
+    const readTasks = loadDB();
 
-    if(readTasks){
+    if (readTasks) {
         tareas.loadTaskFromArray(readTasks);
     }
-    
-    do{
-        opt = await inquirerMenu();  
+
+    do {
+        opt = await inquirerMenu();
         switch (opt) {
             case '1':
                 const desc = await leerInput('Descripcion :');
                 tareas.crearTarea(desc);
-            break;
+                break;
 
             case '2':
                 tareas.allTasksList();
-            break;
+                break;
 
             case '3':
                 tareas.taskCompletedPending(true);
-            break;
+                break;
 
             case '4':
                 tareas.taskCompletedPending(false);
-            break;
+                break;
 
             case '6':
                 const id = await listTaskToDelete(tareas.listadoArr);
-                console.log(id);
-            break;
+
+                if (id !== 0) {
+                    const confirm = await confirmTaskToDelete(
+                        '¿Esta seguro que desea borrar la tarea?'
+                    );
+                    if (confirm) {
+                        tareas.deleteTask(id);
+                        console.log('Tarea borrada correctamente.');
+                    }
+                }
+
+                break;
         }
 
         saveDB(tareas.listadoArr);
-        
-        await pausarApp();     
-    }while (opt !== '0');
-}
+
+        await pausarApp();
+    } while (opt !== '0');
+};
 
 main();
